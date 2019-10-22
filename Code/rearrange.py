ee2083a1e2bd23c95a04ae83c21f28dc23a2c29d
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-10-21 17:25:29 -0700

    stretch - added reverse function

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -4,6 +4,12 @@ import random
 def random_params(params_list):
     return random.sample(params_list, k=len(params_list))
 
+def reverse_word(word):
+    return word[::-1]
+
+def reverse_sentences(sen_list):
+    return
+
 params = sys.argv[1:]
 # print(params)
 
@@ -12,3 +18,6 @@ ran_params_string = ""
 for param in ran_params:
     ran_params_string += param + " "
 print(ran_params_string)
+
+rev_word = reverse_word(random.choice(ran_params))
+print(rev_word)
