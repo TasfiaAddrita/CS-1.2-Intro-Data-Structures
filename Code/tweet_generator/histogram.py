f97b752feb9cdc651dfc931dcfd5d349efafd6b2
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-10-27 22:06:52 -0700

    completed histogram function

--- Code/tweet_generator/histogram.py
+++ Code/tweet_generator/histogram.py
@@ -1,11 +1,23 @@
+import re
+import string
+
 def histogram(source_text):
+
     text = open(source_text)
-    text = text.split()
+    story = text.read()
     text.close()
+
+    # regex help
+    # https://stackoverflow.com/a/13184791/12049271
+    delimiters = ' ', '\n'
+    regexPattern = '|'.join(map(re.escape, delimiters))
+    words = re.split(regexPattern, story)
+    words = list(filter(None, [word.strip(string.punctuation).lower() for word in words]))
+    # print(words)
 
     histogram = {}
 
-    for word in text:
+    for word in words:
         if word not in histogram:
             histogram[word] = 1
         else:
@@ -18,3 +30,6 @@ def unique_words(histogram):
 
 def frequency(word, histogram):
     pass
+
+if __name__ == "__main__":
+    print(histogram('three_wishes.txt'))
