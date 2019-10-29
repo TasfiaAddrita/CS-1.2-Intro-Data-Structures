8ebb6ac4df146a7709c2df2437a0d9f3cd5c5bf9
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-10-28 18:28:04 -0700

    completed lists of lists histogram

--- Code/tweet_generator_tutorial/histogram.py
+++ Code/tweet_generator_tutorial/histogram.py
@@ -1,29 +1,43 @@
 import re
 import string
 
-def histogram(source_text):
-
+def get_words(source_text):
     text = open(source_text)
     story = text.read()
     text.close()
 
-    # regex help
-    # https://stackoverflow.com/a/13184791/12049271
+    # regex help https://stackoverflow.com/a/13184791/12049271
     delimiters = ' ', '\n'
     regexPattern = '|'.join(map(re.escape, delimiters))
     words = re.split(regexPattern, story)
     words = list(filter(None, [word.strip(string.punctuation).lower() for word in words]))
-    # print(words)
+    return words
 
+def histogram_dict(words):
     histogram = {}
-
     for word in words:
         if word not in histogram:
             histogram[word] = 1
         else:
             histogram[word] += 1
-    
     return histogram
+
+def histogram_list(words):
+    histogram = []
+    in_list = False
+
+    for word in words:
+        for w in histogram:
+            if word == w[0]:
+                in_list = True
+                w[1] += 1
+        if in_list == False:
+            histogram.append([word, 1])
+
+    return histogram
+
+def histogram_tuple(words):
+    pass
 
 def unique_words(histogram):
     # easy
@@ -40,6 +54,9 @@ def frequency(word, histogram):
     return histogram[word]
 
 if __name__ == "__main__":
-    hist = histogram('test.txt')
-    print(unique_words(hist))
-    print(frequency('hi', hist))
+    words = get_words('test.txt')
+    hist_dict = histogram_dict(words)
+    print(histogram_list(words))
+    # print(unique_words(hist))
+    # print(frequency('hi', hist))
+    # print(hist)
