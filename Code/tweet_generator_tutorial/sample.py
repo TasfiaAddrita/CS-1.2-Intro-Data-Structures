090d2242e1ddfdf132c07317bb72d248083ec32a
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-10-29 23:57:09 -0700

    cleaned up code

--- Code/tweet_generator_tutorial/sample.py
+++ Code/tweet_generator_tutorial/sample.py
@@ -1,16 +1,20 @@
 import random
 import histogram
+import sys
 
 def random_word(source_text):
     words_list = histogram.get_words(source_text)
     return random.choice(words_list)
+
+def get_freq_percent(word_occurance):
+    return round((word_occurance / len(words_list)) * 100)
 
 def get_word_distribution(words_list):
     histo = histogram.histogram_dict(words_list)
     word_distribution = {}
     range_start = 0
     for word in histo:
-        freq_percent = round((histo[word] / len(words_list)) * 100)
+        freq_percent = get_freq_percent(histo[word])
         word_distribution[word] = (freq_percent, list(range(range_start, range_start + freq_percent + 1)))
         range_start += freq_percent
     
@@ -34,11 +38,14 @@ def test(words_list):
     hist_ran_words = histogram.histogram_dict(test)
 
     for word in hist_ran_words:
-        print('test', word, hist_ran_words[word]/1000)
-        print('dist', word, word_distribution[word][0]/100)
+        print(word, 'test', hist_ran_words[word]/1000, 'dist', word_distribution[word][0]/100)
+        # print('dist', word, word_distribution[word][0]/100)
     
     return
 
-words_list = histogram.get_words('test.txt')
-# print(sample(words_list))
-print(test(words_list))
+# words_list = histogram.get_words('test.txt')
+# test(words_list)
+
+params = sys.argv[1:]
+words_list = histogram.get_words(params[0])
+print(sample(words_list))
