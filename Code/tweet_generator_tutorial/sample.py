f242b7786d10661cc64aa9d177ba4b981151dd18
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-11-03 15:33:39 -0800

    fixed code

--- Code/tweet_generator_tutorial/sample.py
+++ Code/tweet_generator_tutorial/sample.py
@@ -7,7 +7,7 @@ def random_word(source_text):
     return random.choice(words_list)
 
 def get_freq_percent(word_occurance):
-    return round((word_occurance / len(words_list)) * 100)
+    return word_occurance / len(words_list)
 
 def get_word_distribution(words_list):
     histo = histogram.histogram_dict(words_list)
@@ -15,37 +15,62 @@ def get_word_distribution(words_list):
     range_start = 0
     for word in histo:
         freq_percent = get_freq_percent(histo[word])
-        word_distribution[word] = (freq_percent, list(range(range_start, range_start + freq_percent + 1)))
+        word_distribution[word] = [range_start, range_start + freq_percent]
         range_start += freq_percent
     
     return word_distribution
 
 def sample(words_list):
     word_distribution = get_word_distribution(words_list)
-
-    ran_num = random.randint(0, 100)
+    ran_num = random.random()
     for word in word_distribution:
-        if ran_num in word_distribution[word][1]:
+        if word_distribution[word][0] <= ran_num <= word_distribution[word][1]:
             return word
-        # else:
-        #     return (None, ran_num) # ask Alan why expression doesn't think ran_num is in word_distribution
 
 def test(words_list):
     test = []
     word_distribution = get_word_distribution(words_list)
+    print(word_distribution, '\n')
     for _ in range(1000):
         test.append(sample(words_list))
     hist_ran_words = histogram.histogram_dict(test)
-
+    print(hist_ran_words, '\n')
     for word in hist_ran_words:
-        print(word, 'test', hist_ran_words[word]/1000, 'dist', word_distribution[word][0]/100)
-        # print('dist', word, word_distribution[word][0]/100)
-    
+        print(word, 'test', hist_ran_words[word]/1000, 'dist', round(get_freq_percent(histogram.histogram_dict(words_list)[word]), 3))    
     return
 
-# words_list = histogram.get_words('test.txt')
-# test(words_list)
+if __name__ == '__main__':
 
-params = sys.argv[1:]
-words_list = histogram.get_words(params[0])
-print(sample(words_list))
+    # words_list = histogram.get_words('test.txt')
+    # test(words_list)
+
+    params = sys.argv[1:]
+    words_list = histogram.get_words(params[0])
+    # print(sample(words_list))
+    print(test(words_list))
+
+# ------ bad code ------
+# def get_freq_percent(word_occurance):
+#     return round(word_occurance / len(words_list) * 100)
+
+# def get_word_distribution(words_list):
+#     histo = histogram.histogram_dict(words_list)
+#     word_distribution = {}
+#     range_start = 0
+#     for word in histo:
+#         freq_percent = get_freq_percent(histo[word])
+#         word_distribution[word] = (freq_percent, list(range(range_start, range_start + freq_percent + 1)))
+#         range_start += freq_percent
+    
+#     # print(word_distribution)
+#     return word_distribution
+
+# def sample(words_list):
+#     word_distribution = get_word_distribution(words_list)
+
+#     ran_num = random.randint(0, 100)
+#     for word in word_distribution:
+#         if ran_num in word_distribution[word][1]:
+#             return word
+#         # else:
+#         #     return (None, ran_num) # ask Alan why expression doesn't think ran_num is in word_distribution
